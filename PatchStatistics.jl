4767cc84e76a2f62035ab4342a306f05ff46d218
jtardanico <jtard002@fiu.edu>
2020-06-26 00:31:41 +0200

    More additions to PatchStatistics

--- PatchStatistics.jl
+++ PatchStatistics.jl
@@ -5,22 +5,38 @@
 #----------------------------------------------------------------------
 using DataFrames
 using CSV
+using Plots
 
 # Calculation functions
-function tfit(T_opt,T_sd,H_opt,H_sd, temp_t, habitat, α)
-    fitness = exp(-(temp_t-t_opt)^2/(2*T_sd^2)) * exp(-t_sd^2/(2*α^2))
+function tfit(t_opt,t_sd,temp_t, α)
+    fitness = exp(-(temp_t-t_opt)^2/(2*t_sd^2)) * exp(-t_sd^2/(2*α^2))
     return fitness
 end
 
-function hfit(T_opt,T_sd,H_opt,H_sd, temp_t, habitat, α)
-    fitness = exp(-(habitat-h_opt)^2/(2*H_sd^2)) * exp(-h_sd^2/(2*α^2))
+function hfit(h_opt,h_sd,habitat,α)
+    fitness = exp(-(habitat-h_opt)^2/(2*h_sd^2)) * exp(-h_sd^2/(2*α^2))
     return fitness
 end
 
-function fitness(T_opt,T_sd,H_opt,H_sd, temp_t, habitat, α)
-    fitness =  exp(-(temp_t-t_opt)^2/(2*T_sd^2)) * exp(-(habitat-h_opt)^2/(2*H_sd^2)) * exp(-t_sd^2/(2*α^2)) * exp(-h_sd^2/(2*α^2))
+function fitness(t_opt,t_sd,h_opt,h_sd, temp_t, habitat, α)
+    fitness =  exp(-(temp_t-t_opt)^2/(2*t_sd^2)) * exp(-(habitat-h_opt)^2/(2*h_sd^2)) * exp(-t_sd^2/(2*α^2)) * exp(-h_sd^2/(2*α^2))
     return fitness
 end
+
+function shannon(i)
+    p = i ./ sum(i[1:length(i)])
+    h = -sum(p.*log.(p))
+    return h
+end
+
+function shannon(i)
+    p = i ./ sum(i[1:length(i)])
+    λ =  sum(p.^2)
+    return λ
+end
+
+#--------------------------------------------------------
+# Main script
 
 data = DataFrame(CSV.File("/home/joseph/Documents/PhD/LandklifModel/Landscapes/Model1/Experiment1/test5/S0_G0.5_B50_T50_UT_Ae0.0.txt"))
 
@@ -32,22 +48,66 @@ data.H_h = replace!(data.H_h,"NaN" => "Uniform")
 
 # Patch population sizes
 
-populations = by(data, [:Timestep,:x,:y]) do data
+populations = by(data, [:Timestep,:x,:y,:alpha]) do data
     DataFrame(pop=length(data.ID))
 end
 
-rich = by(data, [:Timestep,:x,:y]) do data
+rich = by(data, [:Timestep,:x,:y,:alpha]) do data
     DataFrame(richness=length(unique(data.LineageID)))
 end
 
-ft = by(data, [:Timestep,:x,:y]) do data
-    DataFrame(ft=mean(data.))
+data.temp_t = data.temp_t.+12.5
 
+data.tfit = tfit.(data.T_opt,data.T_sd,data.temp_t,data.alpha)
+data.hfit = hfit.(data.H_opt,data.H_sd,data.habitat,data.alpha)
+data.fit = fitness.(data.T_opt,data.H_opt,data.T_sd,data.H_sd,data.temp_t,data.habitat,data.alpha)
 
-#div = select(data,[:Timestep,:x,:y,:LineageID])
-#div.tally = 1
-#div = by(div,[:Timestep,:x,:y,:LineageID]) do div
-#    DataFrame(count=sum(div.tally))
-#end
-#rich = by(div,[:Timestep,:x,:y]) do div
-#    DataFrame(rich=sum())
+ft = by(data, [:Timestep,:x,:y,:alpha]) do data
+    DataFrame(ft=mean(data.tfit))
+end
+
+fh = by(data, [:Timestep,:x,:y,:alpha]) do data
+    DataFrame(ft=mean(data.hfit))
+end
+
+f = by(data, [:Timestep,:x,:y,:alpha]) do data
+    DataFrame(ft=mean(data.fit))
+end
+
+topt = by(data, [:Timestep,:x,:y,:alpha]) do data
+    DataFrame(T_opt=mean(data.T_opt))
+end
+
+tsd = by(data, [:Timestep,:x,:y,:alpha]) do data
+    DataFrame(T_sd=mean(data.T_sd))
+end
+
+hopt = by(data, [:Timestep,:x,:y,:alpha]) do data
+    DataFrame(H_opt=mean(data.H_opt))
+end
+
+hsd = by(data, [:Timestep,:x,:y,:alpha]) do data
+    DataFrame(H_sd=mean(data.H_sd))
+end
+
+displ = by(data, [:Timestep,:x,:y,:alpha]) do data
+    DataFrame(disp_l=mean(data.disp_l))
+end
+
+dispg = by(data, [:Timestep,:x,:y,:alpha]) do data
+    DataFrame(disp_g=mean(data.disp_g))
+end
+
+fert = by(data, [:Timestep,:x,:y,:alpha]) do data
+    DataFrame(fert=mean(data.fert_max))
+end
+
+div = select(data,[:Timestep,:x,:y,:alpha,:LineageID])
+div.tally = 1
+div = by(div,[:Timestep,:x,:y,:alpha,:LineageID]) do div
+    DataFrame(count=sum(div.tally))
+end
+
+shan = by(div,[:Timestep,:x,:y,:alpha]) do div
+    DataFrame(shannon=shannon(div.count))
+end
